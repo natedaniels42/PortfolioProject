69b6ba40942dd7a38ea7d5dbf683316cd275b0b8
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-06 12:15:17 -0600

    Added projects page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
   {path: '', redirectTo:'home', pathMatch:'full'}
 ];
 
