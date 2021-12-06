ca0e327e106e5e7d976631bb515e41b62b316ccd
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-06 12:29:15 -0600

    Added contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TechComponent } from './tech/tech.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'technologies', component: TechComponent},
+  {path: 'contact', component: ContactComponent},
   {path: '', redirectTo:'home', pathMatch:'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TechComponent } from './tech/tech.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TechComponent } from './tech/tech.component';
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
-    TechComponent
+    TechComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
