4efd913f1f3b5743e753c694281d2c9f3cc971ad
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-09 11:08:20 -0600

    Added conditional to setSource to prevent bugs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,21 +9,26 @@ export class AboutComponent implements OnInit {
   source: string = '';
   view: boolean = false;
   index: number = 0;
+  active: boolean = false;
   constructor() { }
 
   setSource(event: Event) {
-    this.source = (event.target as HTMLImageElement).src;
-    this.view = true;
-
-    const timer = setInterval(() => {
-      this.index++;
-      console.log(this.index);
-      if (this.index === 5) {
-        this.view = false;
-        clearInterval(timer);
-        this.index = 0;
-      }
-    }, 1000)
+    if (!this.active) {
+      this.source = (event.target as HTMLImageElement).src;
+      this.view = true;
+      this.active = true;
+  
+      const timer = setInterval(() => {
+        this.index++;
+        console.log(this.index);
+        if (this.index === 5) {
+          this.view = false;
+          clearInterval(timer);
+          this.index = 0;
+          this.active = false;
+        }
+      }, 1000)
+    }
   }
 
   ngOnInit(): void {
