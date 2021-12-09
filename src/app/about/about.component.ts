3408a5f33e95a17d5c20f54d77c52014abbe1841
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-09 11:02:26 -0600

    Added photo animation to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  source: string = '';
+  view: boolean = false;
+  index: number = 0;
   constructor() { }
+
+  setSource(event: Event) {
+    this.source = (event.target as HTMLImageElement).src;
+    this.view = true;
+
+    const timer = setInterval(() => {
+      this.index++;
+      console.log(this.index);
+      if (this.index === 5) {
+        this.view = false;
+        clearInterval(timer);
+        this.index = 0;
+      }
+    }, 1000)
+  }
 
   ngOnInit(): void {
   }
